7790c8617b2328c2078ff9ed323f6c19056caa3c
Mudit Agarwal <muditag85@gmail.com>
2026-06-07 21:18:31 +0530

    Update product catalogue

--- data/products.js
+++ data/products.js
@@ -55,5 +55,40 @@ var PRODUCTS = [
       "1AV_1larXWec_pVUFnHdaZdzgDAiQmQLK"
     ],
     "stock": 12
+  },
+  {
+    "id": "prod-009",
+    "name": "Scratch Book",
+    "category": "Art & Craft",
+    "price": 45,
+    "description": "",
+    "images": [
+      "1fW3n70KubtaT13d2s1F7uQUDdYqk5u7q",
+      "1z7plaG1Xpl4JQNsrY-FyhaWwnHZqOXrS"
+    ],
+    "stock": 20
+  },
+  {
+    "id": "prod-010",
+    "name": "Tattoo Book",
+    "category": "Art & Craft",
+    "price": 25,
+    "description": "",
+    "images": [
+      "1ttjTUV6CqNIQ5z-fbwBnpNUXSqa78xli"
+    ],
+    "stock": 40
+  },
+  {
+    "id": "prod-011",
+    "name": "Character Pencil Pouch",
+    "category": "Stationery Sets",
+    "price": 35,
+    "description": "",
+    "images": [
+      "1GetwgZU0gaiBQA2I5m80kjgtcQdCNWPx",
+      "1zgAd6I-2ddEThioxeC3rZWopdPU3pzkb"
+    ],
+    "stock": 12
   }
 ];
