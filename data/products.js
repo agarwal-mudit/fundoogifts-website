d26b1954d3237f4868adff223afc2a40e598c2b5
Mudit Agarwal <muditag85@gmail.com>
2026-06-07 21:10:57 +0530

    Update product catalogue

--- data/products.js
+++ data/products.js
@@ -53,5 +53,40 @@ var PRODUCTS = [
       "1KC5p07nO2UHrPK7K1AwryZPcwq2qiVmw"
     ],
     "stock": 20
+  },
+  {
+    "id": "prod-006",
+    "name": "Harry Potter Key Chain",
+    "category": "Keychains",
+    "price": 75,
+    "description": "",
+    "images": [
+      "13eXnjYVKUGmGXtoKXBFp44OuJ6L8BQ6Y"
+    ],
+    "stock": 10
+  },
+  {
+    "id": "prod-007",
+    "name": "Kuromi Key Chain",
+    "category": "Keychains",
+    "price": 75,
+    "description": "",
+    "images": [
+      "16qWStHMH2RSiTJyEXA0jkQVNn5L5l-3Q"
+    ],
+    "stock": 10
+  },
+  {
+    "id": "prod-008",
+    "name": "Camer Projector Key Chain",
+    "category": "Keychains",
+    "price": 80,
+    "description": "",
+    "images": [
+      "1CbVxStofiW_WXm-ofbLWHeMb-mvMrJ0x",
+      "1h_vpY9gjEvrvuHCs0EZVNAImSxrNjNy-",
+      "1AV_1larXWec_pVUFnHdaZdzgDAiQmQLK"
+    ],
+    "stock": 12
   }
 ];
