67ed4420a047cbb8b7bae2bc8eaab6ec97ea9763
Mudit Agarwal <muditag85@gmail.com>
2026-06-07 22:37:48 +0530

    Update product catalogue

--- data/products.js
+++ data/products.js
@@ -90,5 +90,164 @@ var PRODUCTS = [
       "1zgAd6I-2ddEThioxeC3rZWopdPU3pzkb"
     ],
     "stock": 12
+  },
+  {
+    "id": "prod-012",
+    "name": "Acrylic Marker set of 12",
+    "category": "Stationery Sets",
+    "price": 99,
+    "description": "",
+    "images": [
+      "1t7ulf0pP7GrbL5Yz4-QuJIfghru8FnAg",
+      "1R7H-JpFV_PeRPzsB0mncZp1eYQ1V8wco"
+    ],
+    "stock": 24
+  },
+  {
+    "id": "prod-013",
+    "name": "Bottle shaped Highlighter",
+    "category": "Art & Craft",
+    "price": 99,
+    "description": "",
+    "images": [
+      "190Y6fLrOUQkk4K9KszEqbhfJiXsABX3-",
+      "18Z1g379Uw2_3zKtdcz9O1yxsh1mYvv6D"
+    ],
+    "stock": 12
+  },
+  {
+    "id": "prod-014",
+    "name": "Character Pencil",
+    "category": "Stationery Sets",
+    "price": 20,
+    "description": "",
+    "images": [
+      "1ZkBkvloinI8jFr3TfCocYebEBAd46KFB",
+      "1g5zEx-B_t_FDYPF4DPQbxKjEXpAibQ3S"
+    ],
+    "stock": 48
+  },
+  {
+    "id": "prod-015",
+    "name": "Diary with Lock",
+    "category": "Stationery Sets",
+    "price": 80,
+    "description": "",
+    "images": [
+      "1a3wo-OPP_zhX8grDwPmrd0FScPOSYJOV"
+    ],
+    "stock": 24
+  },
+  {
+    "id": "prod-016",
+    "name": "Diary with Pen",
+    "category": "Stationery Sets",
+    "price": 80,
+    "description": "",
+    "images": [
+      "14CGlL-dhII3-jRbQP_CNI_b8p7cf3prF"
+    ],
+    "stock": 16
+  },
+  {
+    "id": "prod-017",
+    "name": "Dual side Markers",
+    "category": "Art & Craft",
+    "price": 150,
+    "description": "Set of 12",
+    "images": [
+      "1SSWdI1pVj2xcX6AHmeAyJuamjYcMC96t"
+    ],
+    "stock": 12
+  },
+  {
+    "id": "prod-018",
+    "name": "Fur Diary",
+    "category": "Stationery Sets",
+    "price": 190,
+    "description": "",
+    "images": [
+      "1UkrZAI4Ebpw1JTZvMn6zIvv5WHCtoFJm"
+    ],
+    "stock": 12
+  },
+  {
+    "id": "prod-019",
+    "name": "Keyboard Keychain",
+    "category": "Keychains",
+    "price": 180,
+    "description": "",
+    "images": [
+      "1nSxsIc6ay4s4mlxL4o6BWKQNApKUZk2B"
+    ],
+    "stock": 12
+  },
+  {
+    "id": "prod-020",
+    "name": "Magic Water Book",
+    "category": "Art & Craft",
+    "price": 60,
+    "description": "",
+    "images": [
+      "1_Z7QQzmrejk9JhslY0_XBdKIVZBUX2Mx"
+    ],
+    "stock": 24
+  },
+  {
+    "id": "prod-021",
+    "name": "Mini Stationery set",
+    "category": "Stationery Sets",
+    "price": 99,
+    "description": "",
+    "images": [
+      "1EhqWlZ46ch2lsJQSlBZHdvl2gv535jP9"
+    ],
+    "stock": 20
+  },
+  {
+    "id": "prod-022",
+    "name": "PopIT Diary",
+    "category": "Stationery Sets",
+    "price": 199,
+    "description": "",
+    "images": [
+      "19NdXIYDGWV0SEQuC6iJDmPPeqS20_P-x",
+      "1lHyEWuUN5ebEPYf2jeT7UsZFM8Hzswac"
+    ],
+    "stock": 12
+  },
+  {
+    "id": "prod-023",
+    "name": "Rope Game",
+    "category": "Puzzle & Games",
+    "price": 140,
+    "description": "",
+    "images": [
+      "1NPp2XFJFnfUO8-2NWnD3asOREyuMR5FX"
+    ],
+    "stock": 14
+  },
+  {
+    "id": "prod-024",
+    "name": "Snowy 2-in-1 Writing Board",
+    "category": "Stationery Sets",
+    "price": 120,
+    "description": "",
+    "images": [
+      "15Adp39uHrdLlSJO6N8Jc9PDrRCtQV50V"
+    ],
+    "stock": 12
+  },
+  {
+    "id": "prod-025",
+    "name": "Fundoo Stamps",
+    "category": "Art & Craft",
+    "price": 99,
+    "description": "",
+    "images": [
+      "1HStvnLFZMX1J8WlJ0zqhUX1s5IB4yem3",
+      "159xbansbg0rjnMVJ1sVhWNLsVpnAUlsA"
+    ],
+    "stock": 24
   }
 ];
